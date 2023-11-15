b1be248877c71da625051f54524e3d7e382ca9ff
Lobzang Yonten <Lobzangyenten1@gmail.com>
2023-11-16 00:35:42 +0600

    staffInfo

--- src/Pages/ExamCell/StaffInfo.jsx
+++ src/Pages/ExamCell/StaffInfo.jsx
@@ -3,6 +3,15 @@ import "./resultView.css";
 import { DownloadTableExcel } from "react-export-table-to-excel";
 import { useStateContext } from "../../Contexts/ContextProvider";
 import Loader from "react-js-loader";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Search,
+  Inject,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
 
 const StaffInfo = ({ department, year, semester }) => {
   const tableRef = useRef(null);
@@ -43,42 +52,20 @@ const StaffInfo = ({ department, year, semester }) => {
 
   const valst = "px-2 py-1 whitespace-nowrap  text-xs font-medium text-black ";
   return (
-    <div className="m-4 ">
-      <table className="dark:bg-white min-w-full divide-y divide-gray-200">
-        <thead>
-          <tr>
-            <th className="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-black">
-              Tutor ID
-            </th>
-            <th className="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-black">
-              Name
-            </th>
-            <th className="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-black">
-              Department
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((tutor, index) => (
-            <tr key={index}>
-              <td className={valst}>{tutor.id}</td>
-              <td className={valst}>{tutor.name}</td>
-              <td className={valst}>{tutor.department}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div>
-        <DownloadTableExcel
-          filename={`${year} ${department}`}
-          sheet="users"
-          currentTableRef={tableRef.current}
-        >
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Export Excel
-          </button>
-        </DownloadTableExcel>
-      </div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <p className="text-2xl font-bold mb-4">Staft Information</p>
+      <GridComponent
+        dataSource={data}
+        allowPaging
+        allowSorting
+        toolbar={["Search"]}
+        width="auto"
+      >
+        {/* <ColumnsDirective>
+        {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+      </ColumnsDirective> */}
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
     </div>
   );
 };
